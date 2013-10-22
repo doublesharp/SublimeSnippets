badd85adab8cb83893744ee48c24c2ca23f633ae
doublesharp <jsilver@doublesharp.com>
2013-10-22 11:11:06 -0700

    Update command structure

--- mycached.py
+++ mycached.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin, os, json
 
-class MyCachedCommand(sublime_plugin.TextCommand):
+class MycachedCommand(sublime_plugin.TextCommand):
     values_file, key = 'User/mycached-values.json', None
     def run(self, edit, action, key=''):
         if action == "add":
@@ -13,7 +13,7 @@ class MyCachedCommand(sublime_plugin.TextCommand):
                     if action == "copy" or action == "cast":
                         sublime.set_clipboard(key_values[menu_items[index]])
                     if action == "paste" or action == "cast": 
-                        sublime.active_window().run_command('my_cached_paste', {'value':key_values[menu_items[index]]})
+                        sublime.active_window().run_command('mycached_paste', {'value':key_values[menu_items[index]]})
                     if action == "edit": 
                         self.on_add_key(menu_items[index]);
             self.view.window().show_quick_panel(menu_items, on_select)
@@ -22,7 +22,7 @@ class MyCachedCommand(sublime_plugin.TextCommand):
         if self.key in values: value = values[self.key]
         sublime.active_window().show_input_panel("Please {} the value for '{}':".format('enter' if not value else 'update', self.key), value, self.on_add_value, None, self.cancel_add_value)
     def cancel_add_value(self):
-        sublime.active_window().run_command('my_cached', {'action':'add', 'key':self.key})
+        sublime.active_window().run_command('mycached', {'action':'add', 'key':self.key})
     def on_add_value(self, value):
         values, values[self.key], src = self.get_key_values(), value, open(os.path.join(sublime.packages_path(), self.values_file), "w")
         write, close = src.write(json.dumps(values, sort_keys=True, indent=4)), src.close()
@@ -34,7 +34,7 @@ class MyCachedCommand(sublime_plugin.TextCommand):
             data = { "No Results": "" } if ui else {}
         return data
 
-class MyCachedPasteCommand(sublime_plugin.TextCommand):
+class MycachedPasteCommand(sublime_plugin.TextCommand):
     def run(self, edit, value):
         view = sublime.active_window().active_view()
         view.insert(edit, view.sel()[0].begin(), value)
